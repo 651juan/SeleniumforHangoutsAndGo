099429189159dd9fa12e3cb7e747ee12726f71ee
Adrian <apapadrian@gmail.com>
2015-11-27 23:42:39 +0100

    Added Settings Menu Shortcuts Saving/loading settings

--- ICS2202/src/BlindAid.java
+++ ICS2202/src/BlindAid.java
@@ -2,7 +2,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import javax.swing.*;
@@ -10,12 +9,27 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.List;
 
-public class BlindAid extends JFrame implements ActionListener {
+public class BlindAid extends JFrame implements ActionListener, KeyListener {
 	/**
 	 * Variables
 	 */
+	private String[] callNumbers = new String[9];
+	private String[] smsNumbers = new String[9];
+	private boolean goProvider = false;
+	private boolean vodafoneProvider = false;
+	
+	private boolean controlPressed = false;
+	private boolean cPressed = false;
+	private boolean mPressed = false;
+
 	private Container container;
 	private JMenu FILE_MENU;
 	private String MESSAGING_PROVIDER = "Vodafone";
@@ -25,8 +39,8 @@ public class BlindAid extends JFrame implements ActionListener {
 	private String goPassword = "";
 	private String vodafoneUsername = "";
 	private String vodafonePassword = "";
-	String phoneNumber;
-	WebDriver driver;
+	private WebDriver driver;
+	private WebDriverWait wait;
 
 	/**
 	 * Create the frame.
@@ -35,8 +49,24 @@ public class BlindAid extends JFrame implements ActionListener {
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setBounds(100, 100, 400, 150);
 		this.setResizable(false);
+		this.addKeyListener(this);
+        this.setFocusable(true);
 		container = this.getContentPane();
 		container.setLayout(new GridLayout(1,2));
+
+		try {
+			File inputFile = new File("data.dat");
+			if(inputFile.exists()){
+				ObjectInputStream ois = new ObjectInputStream(new FileInputStream(inputFile));
+				this.goProvider = ois.readBoolean();
+				this.vodafoneProvider = ois.readBoolean();
+				this.callNumbers = (String[]) ois.readObject();
+				this.smsNumbers = (String[]) ois.readObject();
+				ois.close(); 
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
 
 		//Create an instance of a menu Bar
 		JMenuBar menuBar = new JMenuBar();
@@ -87,18 +117,210 @@ public class BlindAid extends JFrame implements ActionListener {
 		menuItem.addActionListener(this);
 		FILE_MENU.add(menuItem);
 	}
+	
+	@Override
+	public void keyPressed(KeyEvent e) {
+		if(e.getKeyCode() == KeyEvent.VK_CONTROL) {
+			this.controlPressed = true;
+		}
+		
+		if(e.getKeyCode() == KeyEvent.VK_C) {
+			this.cPressed = true;
+		}
+		
+		if(e.getKeyCode() == KeyEvent.VK_M) {
+			this.mPressed = true;
+		}
+		
+		if(controlPressed) {
+			if(e.getKeyCode() == KeyEvent.VK_1){
+				if(mPressed){
+					this.sms(smsNumbers[1]);
+				}else if(cPressed){
+					this.call(callNumbers[1]);
+				}
+			}else if(e.getKeyCode() == KeyEvent.VK_2){
+				if(mPressed){
+					this.sms(smsNumbers[2]);
+				}else if(cPressed){
+					this.call(callNumbers[2]);
+				}
+			}else if(e.getKeyCode() == KeyEvent.VK_3){
+				if(mPressed){
+					this.sms(smsNumbers[3]);
+				}else if(cPressed){
+					this.call(callNumbers[3]);
+				}
+			}else if(e.getKeyCode() == KeyEvent.VK_4){
+				if(mPressed){
+					this.sms(smsNumbers[4]);
+				}else if(cPressed){
+					this.call(callNumbers[4]);
+				}
+			}else if(e.getKeyCode() == KeyEvent.VK_5){
+				if(mPressed){
+					this.sms(smsNumbers[5]);
+				}else if(cPressed){
+					this.call(callNumbers[5]);
+				}
+			}else if(e.getKeyCode() == KeyEvent.VK_6){
+				if(mPressed){
+					this.sms(smsNumbers[6]);
+				}else if(cPressed){
+					this.call(callNumbers[6]);
+				}
+			}else if(e.getKeyCode() == KeyEvent.VK_7){
+				if(mPressed){
+					this.sms(smsNumbers[7]);
+				}else if(cPressed){
+					this.call(callNumbers[7]);
+				}
+			}else if(e.getKeyCode() == KeyEvent.VK_8){
+				if(mPressed){
+					this.sms(smsNumbers[8]);
+				}else if(cPressed){
+					this.call(callNumbers[8]);
+				}
+			}else if(e.getKeyCode() == KeyEvent.VK_9){
+				if(mPressed){
+					this.sms(smsNumbers[9]);
+				}else if(cPressed){
+					this.call(callNumbers[9]);
+				}
+			}
+		}
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		if(e.getKeyCode() == KeyEvent.VK_CONTROL) {
+			this.controlPressed = false;
+		}
+		
+		if(e.getKeyCode() == KeyEvent.VK_C) {
+			this.cPressed = false;
+		}
+		
+		if(e.getKeyCode() == KeyEvent.VK_C) {
+			this.mPressed = false;
+		}
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {}
 
 	@Override
 	public void actionPerformed(ActionEvent event) {		
-		//Used to wait for objects to load
-		WebDriverWait wait;
-		
+		String phoneNumber;
 		switch( event.getActionCommand()) {
 		case "Call": System.out.println("Call Pressed");
 		//Enter Phone Number to Call
 		phoneNumber = JOptionPane.showInputDialog(this, "Who Would you like to call?");
 		System.out.println("Number to Call: " + phoneNumber);
+		if(phoneNumber != null) {
+			this.call(phoneNumber);
+		}
+		break;
 
+		case "SMS": System.out.println("SMS Pressed");
+		phoneNumber = JOptionPane.showInputDialog(this, "Who Would you like to text?");
+		if(phoneNumber != null) {
+			this.sms(phoneNumber);
+		}
+		break;
+
+		case "Options": System.out.println("Options Selected");	
+		JPanel myPanel = new JPanel();
+		myPanel.setLayout(new GridLayout(0,1));
+
+		JTabbedPane tabContainer = new JTabbedPane();
+
+		//GeneralTab
+		JPanel generalTab = new JPanel();
+		JLabel providerLabel = new JLabel("Provider:");
+		JRadioButton goButton = new JRadioButton("GO Mobile");
+		goButton.setActionCommand("GO Mobile");
+		goButton.setSelected(this.goProvider);
+
+		JRadioButton vodafoneButton = new JRadioButton("Vodafone");
+		vodafoneButton.setActionCommand("Vodafone");
+		vodafoneButton.setSelected(this.vodafoneProvider);
+
+		ButtonGroup radioGroup = new ButtonGroup();
+		radioGroup.add(goButton);
+		radioGroup.add(vodafoneButton);
+
+		generalTab.add(providerLabel);
+		generalTab.add(goButton);
+		generalTab.add(vodafoneButton);
+
+		//CallTab
+		JPanel callTab = new JPanel();
+		JTextField[] callNumBoxes = new JTextField[9];
+		callTab.setLayout(new GridLayout(9,2,5,5));
+
+		for(int i = 0; i < 9; i++) {
+			JLabel callShortcutsLabel = new JLabel("Call Shortcuts CTRL + C + "+i+" ");
+			callTab.add(callShortcutsLabel);
+			callNumBoxes[i] = new JTextField();
+			callNumBoxes[i].setText(this.callNumbers[i]);
+			callTab.add(callNumBoxes[i]);
+		}
+
+		//SMSTab
+		JPanel smsTab = new JPanel();
+		JTextField[] smsNumBoxes = new JTextField[9];
+		smsTab.setLayout(new GridLayout(9,2,5,5));
+
+		for(int i = 0; i < 9; i++) {
+			JLabel callShortcutsLabel = new JLabel("SMS Shortcuts CTRL + M + "+i+" ");
+			smsTab.add(callShortcutsLabel);
+			smsNumBoxes[i] = new JTextField();
+			smsNumBoxes[i].setText(this.smsNumbers[i]);
+			smsTab.add(smsNumBoxes[i]);
+		}
+		tabContainer.add("General", generalTab);
+		tabContainer.add("Call", callTab);
+		tabContainer.add("SMS", smsTab);
+		myPanel.add(tabContainer);
+
+
+		int optionResult = JOptionPane.showConfirmDialog(this, myPanel, "Settings", JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
+
+		if(optionResult == JOptionPane.OK_OPTION) {
+
+			try {
+				for(int i = 0; i < 9; i ++) {
+					this.callNumbers[i] = callNumBoxes[i].getText();
+					this.smsNumbers[i] = smsNumBoxes[i].getText();
+				}
+
+				this.goProvider = goButton.isSelected();
+				this.vodafoneProvider = vodafoneButton.isSelected();
+
+				FileOutputStream fos = new FileOutputStream("data.dat");
+				ObjectOutputStream oos = new ObjectOutputStream(fos);   
+				oos.writeBoolean(goButton.isSelected());
+				oos.writeBoolean(vodafoneButton.isSelected());
+				oos.writeObject(this.callNumbers);
+				oos.writeObject(this.smsNumbers);
+				oos.close(); 
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+		break;
+
+		case "Quit": System.out.println("Quit Selected");
+		//Quit
+		System.exit(0);
+		break;
+		}
+	}
+
+	private void call(String num) {
 		//ChromeOptions options = new ChromeOptions();
 		//options.addArguments("--use-fake-ui-for-media-stream=true");
 		//Create a new driver to control chrome
@@ -155,7 +377,7 @@ public class BlindAid extends JFrame implements ActionListener {
 
 		//Send phone number to input field
 		WebElement numberInputField = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\":4.vw\"]/div/div[1]/div/div[2]/div/div[2]/div/div/div[1]/table/tbody/tr/td/input")));
-		numberInputField.sendKeys(phoneNumber);
+		numberInputField.sendKeys(num);
 		//GetPhoneCall Image
 		WebElement callButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\":4.vw\"]/div/div[2]/div/div/div/div[3]/ul")));
 		callButton.click();
@@ -176,13 +398,11 @@ public class BlindAid extends JFrame implements ActionListener {
 
 		this.waitMS(5000);
 		driver.close();
-		break;
+	}
 
-		case "SMS": System.out.println("SMS Pressed");
-		phoneNumber = JOptionPane.showInputDialog(this, "Who Would you like to text?");
+	private void sms(String num) {
 		String message = JOptionPane.showInputDialog(this, "Enter your message");
-		System.out.println("Number to test: " + phoneNumber + " Message: " + message);
-
+		System.out.println("Number to test: " + num + " Message: " + message);
 		//ChromeOptions options = new ChromeOptions();
 		//options.addArguments("--use-fake-ui-for-media-stream=true");
 		//Create a new driver to control chrome
@@ -210,13 +430,13 @@ public class BlindAid extends JFrame implements ActionListener {
 				driver.findElement(By.className("ui-dialog-titlebar-close")).click();
 			}
 
-			driver.findElement(By.id("web-to-sms-form-recipients")).sendKeys(phoneNumber);
+			driver.findElement(By.id("web-to-sms-form-recipients")).sendKeys(num);
 			driver.findElement(By.id("web-to-sms-form-message")).sendKeys(message);
 			driver.findElement(By.id("web-to-sms-form-submit")).click();
 		}else if(MESSAGING_PROVIDER.equalsIgnoreCase("Vodafone")){
 			//New wait object 10 seconds
 			wait = new WebDriverWait(driver, 10);
-			
+
 			//Visit Vodafone
 			driver.get("https://www.vodafone.com.mt/");
 			wait.until(ExpectedConditions.elementToBeClickable(By.id("ctl00_ctl00_cphParent_ResponsiveQuickLinks_ResponsiveLogin_txtUsername_txtInput"))).sendKeys(this.vodafoneUsername);
@@ -229,27 +449,17 @@ public class BlindAid extends JFrame implements ActionListener {
 				e.printStackTrace();
 			}
 			wait.until(ExpectedConditions.elementToBeClickable(By.id("ctl00_ctl00_cphParent_ResponsiveQuickLinks_ResponsiveLogin_btnLogin"))).click();
-			
+
 			this.waitMS(5000);
 			driver.get("https://www.vodafone.com.mt/web-to-sms");
 
-			wait.until(ExpectedConditions.elementToBeClickable(By.id("ctl00_ctl00_cphParent_cphMain_ctl00_tbvOtherVodafoneNumber_txtInput"))).sendKeys(phoneNumber);
+			wait.until(ExpectedConditions.elementToBeClickable(By.id("ctl00_ctl00_cphParent_cphMain_ctl00_tbvOtherVodafoneNumber_txtInput"))).sendKeys(num);
 			wait.until(ExpectedConditions.elementToBeClickable(By.id("ctl00_ctl00_cphParent_cphMain_ctl00_txtContent_txtInput"))).sendKeys(message);
 			wait.until(ExpectedConditions.elementToBeClickable(By.id("ctl00_ctl00_cphParent_cphMain_ctl00_btnSend"))).click();
 		}
 
 		this.waitMS(10000);
 		driver.close();
-		break;
-
-		case "Options": System.out.println("Options Selected");
-		break;
-
-		case "Quit": System.out.println("Quit Selected");
-		//Quit
-		System.exit(0);
-		break;
-		}
 	}
 
 	/**
